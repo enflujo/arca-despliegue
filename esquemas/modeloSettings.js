8a6ae3169553a957e3f5e364aa09d8fcfc7676c8
Juan Camilo González <j.gonzalezj@uniandes.edu.co>
2022-03-08 10:33:17 -0500

    🚀 Base para automatizar configuración general

---
+++ esquemas/modeloSettings.js
@@ -0,0 +1,12 @@
+export default {
+  settings: {
+    project_name: 'Proyecto Arca',
+    project_url: 'https://proyectoarca.uniandes.edu.co',
+    project_color: '#af2828',
+    public_note: 'Administrador de contenido del proyecto ARCA.',
+    storage_asset_transform: 'presets',
+    project_descriptor: 'CMS',
+  },
+  folders: ['Sistema', 'Obras'],
+  imgsSistema: [{ nombre: 'Icono Arca', ruta: './imgs/arca-icono.svg' }],
+};
